6634d0e28cf9448ef82eb5495d2b9e3a70a62c6b
Xianwei Zhang <Xianwei.Zhang@amd.com>
2019-07-23 11:37:18 -0500

    GCEA: add VG20/MI100 events file and fix parsing
    Compared to VG10, VG20/MI100 adds six more (#77~
    82) GCEA events, which are organized as one per
    line in regspec, differring from VG10 that all
    events are in one single line. This patch adds
    the new GCEA events file, and improves the parser
    script to work on both organization formats.
    
    Change-Id: I90d6d9c6a90f4eee89245d29daced952657eb339

--- script/pmc_ea_extr.py
+++ script/pmc_ea_extr.py
@@ -14,6 +14,23 @@ def write_xml(out, event_name, block, event_id, descr):
   out.write("  <metric\n" +
             "    name=\"" + event_name + "\" block=" + block + " event=" + event_id + " descr=\"" + descr + "\"\n" +
             "  ></metric>\n")
+
+# parse event from the matched string and output to xml
+def parse_event(m, ngroups, pref, pattern_trim, pattern_norm, out, block):
+  assert (ngroups >= 3),"must be at least 3 groups"
+  event_id =  int(m.group(1).strip())
+  event_suff = m.group(2).strip()
+  event_descr = m.group(3).strip()
+  event_name = '_'.join([pref, event_suff, event_descr])
+  event_name = pattern_trim.sub('', event_name)
+  event_name = pattern_norm.sub('_', event_name)
+  event_name = pattern_norm.sub('_', event_name)
+
+  write_xml(out, event_name, block, str(event_id), event_descr)
+  print str(event_id) + ' : ' + event_name
+
+  return event_id
+
 #############################################################
 
 def parse_rai_descr(inp, out, block, pref):
@@ -21,7 +38,10 @@ def parse_rai_descr(inp, out, block, pref):
   #  4 : EA_PERF_rdram_Transaction_start_from_latency_sampler_0 : "Transaction `start` from latency sampler 0"
   #  5 : EA_PERF_Transaction_end_from_latency_sampler_0 : "Transaction `end` from latency sampler 0"
   #pattern = re.compile(r'\s(\d+)\s|\s(\w*)\s?|[^|]*|([^|]*|?)')
+  # pattern: all events in a line
   pattern_rec = re.compile(r'\s+(\d+)\s*\|\s*(\w*)\s*\|[^|]*\|\s*([^|]+)(\s\d+\s*\||$)')
+  # pattern2: one event per line
+  pattern2_rec = re.compile(r'\s*(\d+)\s*\|\s*(\w*)\s*\|[^|]*\|\s*([^|]+)\s*$')
   pattern_trim = re.compile(r'\s\d+\s\|$');
   pattern_norm = re.compile(r'(\W|_+)');
   pos = 0
@@ -32,26 +52,28 @@ def parse_rai_descr(inp, out, block, pref):
     line = line[:-1]
     while pos < len(line):
       m = pattern_rec.search(line, pos)
-      if not m:
-        print >>sys.stderr, "Error: bad line: \"" + line
-        sys.exit(1)
-      else:
-        event_id =  int(m.group(1))
-        event_suff = m.group(2)
-        event_descr = m.group(3)
-        event_name = '_'.join([pref, event_suff, event_descr])
-        event_name = pattern_trim.sub('', event_name)
-        event_name = pattern_norm.sub('_', event_name)
-        event_name = pattern_norm.sub('_', event_name)
-        pos = m.end() - len(m.group(4))
+      m2 = pattern2_rec.search(line, pos)
+      # matching first pattern
+      if m:
+        ngroups = len(m.groups())
+        pos = m.end() - len(m.group(ngroups))
 
-        write_xml(out, event_name, block, str(event_id), event_descr)
-        print str(event_id) + ' : ' + event_name
+        event_id = parse_event(m, ngroups, pref, pattern_trim, pattern_norm, out, block)
 
         if event_id != next_event:
           print >>sys.stderr, "Error: event skipped(" + str(next_event) + ")"
           sys.exit(1)
         next_event = event_id + 1
+      # matching pattern2
+      elif m2:
+        ngroups = len(m2.groups())
+        parse_event(m2, ngroups, pref, pattern_trim, pattern_norm, out, block)
+
+        break
+      # matching neither, skip over
+      else:
+        print >>sys.stderr, "Error: bad line: \"" + line
+        break
 #############################################################
 
 if (len(sys.argv) != 3):
