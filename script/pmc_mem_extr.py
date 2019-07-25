0586a6f6404a5b22e1196b6c0ebe9fc1e3002b09
Xianwei Zhang <Xianwei.Zhang@amd.com>
2019-07-25 11:26:17 -0500

    script: improve GCEA script to support MC_VM_L2
    This patches improves the GCEA script to work for
    MC_VM_L2 as well, and renames to make it general.
    
    Change-Id: I31f2e3299a9b48b1608ea1386b28cfe5791530fe

--- script/pmc_mem_extr.py
+++ script/pmc_mem_extr.py
@@ -16,68 +16,88 @@ def write_xml(out, event_name, block, event_id, descr):
             "  ></metric>\n")
 
 # parse event from the matched string and output to xml
-def parse_event(m, ngroups, pref, pattern_trim, pattern_norm, out, block):
+def parse_event(m, ngroups, pref, pattern_trim, pattern_norm, out, block, nth_event):
   assert (ngroups >= 3),"must be at least 3 groups"
-  event_id =  int(m.group(1).strip())
+  event_ind =  int(m.group(1).strip())
   event_suff = m.group(2).strip()
   event_descr = m.group(3).strip()
-  event_name = '_'.join([pref, event_suff, event_descr])
-  event_name = pattern_trim.sub('', event_name)
-  event_name = pattern_norm.sub('_', event_name)
-  event_name = pattern_norm.sub('_', event_name)
+  if pref and pattern_trim and pattern_norm:
+    # GCEA
+    event_name = '_'.join([pref, event_suff, event_descr])
+    event_name = pattern_trim.sub('', event_name)
+    event_name = pattern_norm.sub('_', event_name)
+    event_name = pattern_norm.sub('_', event_name)
+  else:
+    # others, e.g., MCVML2
+    event_name = event_suff
+    block = event_name.split("_")[0]
 
-  write_xml(out, event_name, block, str(event_id), event_descr)
-  print str(event_id) + ' : ' + event_name
+  # first time, output block info
+  if nth_event == 0:
+    out.write("  # " + block + " counters\n")
 
-  return event_id
+  write_xml(out, event_name, block, str(event_ind), event_descr)
+  print str(event_ind) + ' : ' + event_name
+
+  return event_ind
 
 #############################################################
 
-def parse_rai_descr(inp, out, block, pref):
+def parse_rai_descr(inp, out, block='GCEA', pref='EA_PERF'):
   # 4 | rdram | {3`b0, start0} | Transaction `start` from latency sampler 0 5 | rdram | {3`b0, end0 } | Transaction `end` from latency sampler 0 ...
   #  4 : EA_PERF_rdram_Transaction_start_from_latency_sampler_0 : "Transaction `start` from latency sampler 0"
   #  5 : EA_PERF_Transaction_end_from_latency_sampler_0 : "Transaction `end` from latency sampler 0"
   #pattern = re.compile(r'\s(\d+)\s|\s(\w*)\s?|[^|]*|([^|]*|?)')
-  # pattern: all events in a line
+  # pattern: all events in a line, see (vg10) GCEA_PERFCOUNTER0_CFG.txt
   pattern_rec = re.compile(r'\s+(\d+)\s*\|\s*(\w*)\s*\|[^|]*\|\s*([^|]+)(\s\d+\s*\||$)')
-  # pattern2: one event per line
-  pattern2_rec = re.compile(r'\s*(\d+)\s*\|\s*(\w*)\s*\|[^|]*\|\s*([^|]+)\s*$')
+  # pattern2: one event per line, see VG20_MI100_GCEA_PERFCOUNTER0_CFG.txt
+  pattern2_rec = re.compile(r'^\s*(\d+)\s*\|\s*(\w*)\s*\|[^|]*\|\s*([^|]+)\s*$')
+  # pattern3: one event per line, see MC_VM_L2_PERFCOUNTER0_CFG.txt
+  #   01 - MCVML2_PERF_SEL_BANK0_PTE_CACHE_REQ: number of bank0 pte cache requests
+  pattern3_rec = re.compile(r'^\s*(\d+)\s*-\s*(\w*)\s*:\s*(.*)$')
   pattern_trim = re.compile(r'\s\d+\s\|$');
   pattern_norm = re.compile(r'(\W|_+)');
   pos = 0
   next_event = 0
 
-  out.write("  # " + block + " counters\n")
   for line in inp.readlines():
     line = line[:-1]
     while pos < len(line):
       m = pattern_rec.search(line, pos)
       m2 = pattern2_rec.search(line, pos)
+      m3 = pattern3_rec.search(line, pos)
       # matching first pattern
       if m:
         ngroups = len(m.groups())
         pos = m.end() - len(m.group(ngroups))
 
-        event_id = parse_event(m, ngroups, pref, pattern_trim, pattern_norm, out, block)
+        event_ind = parse_event(m, ngroups, pref, pattern_trim, pattern_norm, out, block, next_event)
 
-        if event_id != next_event:
-          print >>sys.stderr, "Error: event skipped(" + str(next_event) + ")"
-          sys.exit(1)
-        next_event = event_id + 1
+        if event_ind != next_event:
+          print >>sys.stderr, "Warn: event skipped(" + str(next_event) + ")"
+        next_event = event_ind + 1
       # matching pattern2
       elif m2:
         ngroups = len(m2.groups())
-        parse_event(m2, ngroups, pref, pattern_trim, pattern_norm, out, block)
+        parse_event(m2, ngroups, pref, pattern_trim, pattern_norm, out, block, next_event)
+        next_event += 1
 
         break
-      # matching neither, skip over
+      # matching pattern3
+      elif m3:
+        ngroups = len(m3.groups())
+        parse_event(m3, ngroups, None, None, None, out, block, next_event)
+        next_event += 1
+
+        break
+      # matching none, skip over
       else:
-        print >>sys.stderr, "Error: bad line: \"" + line
+        print >>sys.stderr, "Skip line: \"" + line
         break
 #############################################################
 
 if (len(sys.argv) != 3):
-  print >>sys.stderr, "Usage:", sys.argv[0], " <input GCEA description file> <gfxip in lower case, gfx8, gfx9, etc..>"
+  print >>sys.stderr, "Usage:", sys.argv[0], " <input description file> <gfxip in lower case, gfx8, gfx9, etc..>"
   sys.exit(1)
 
 raifile = sys.argv[1]
@@ -93,7 +113,7 @@ out_name = nms + '_' + m.group(1) + '.xml'
 
 inp = open(raifile, 'r')
 out = open_xml(out_name, nms)
-parse_rai_descr(inp, out, 'GCEA', 'EA_PERF')
+parse_rai_descr(inp, out)
 close_xml(out, nms);
 print "generated '" + out_name + "'"
 #############################################################
