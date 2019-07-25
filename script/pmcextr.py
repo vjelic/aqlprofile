c9ddb3f2202f827acd0850b10ab76d35007d345d
Xianwei Zhang <Xianwei.Zhang@amd.com>
2019-07-25 09:58:32 -0500

    scripts: improve script to parse GRBM_SE and SDMA
    Blocks/events for GRBM_SE and SDMA are formatted
    differently from others like TCP in .RAI file,
    and thus were not supported to parse. This patch
    adds the support. In addition, this patch fixes
    the prefix 'SQ'/'SQC' for SQ.
    
    Change-Id: I5ae5cf16ef7f4cba4344a5d48a0fddc11d6d7aaa

--- script/pmcextr.py
+++ script/pmcextr.py
@@ -2,7 +2,7 @@
 import os, sys, re
 
 #blocks_list = [ "CPC", "CPF", "GDS", "GRBM", "GRBMSE", "SPI", "SQ", "SQCS", "SRBM", "SX", "TA", "TCA", "TCC", "TCP", "TD" ]
-blocks_list = [ "CPC", "CPF", "GDS", "GRBM", "SPI", "SQ", "SX", "TA", "TCA", "TCC", "TCP", "TD", "GRBMSE", "SQCS", "SRBM" ]
+blocks_list = [ "CPC", "CPF", "GDS", "GRBM", "SPI", "SQ", "SX", "TA", "TCA", "TCC", "TCP", "TD", "GRBM_SE", "SDMA" ]
 
 REC_MAX_LEN = 1024
 
@@ -25,6 +25,8 @@ def parse_event(rec_pattern, record, block, out):
   # not a match, return
   if not m: return False
 
+  # for SQ block, events start with 'SQ' or 'SQC'
+  if block == "SQ" and m.group(1).startswith('C'): block += 'C'
   event_name = (block + "_" + m.group(2)).upper()
   event_id = int(m.group(4), 0)
   descr = m.group(3);
@@ -38,33 +40,44 @@ def parse_event(rec_pattern, record, block, out):
 #############################################################
 
 def parse_rai(inp, out, blist):
-  end_pattern = re.compile("}")
+  # event block ends with '};' on a single line
+  end_pattern = re.compile("^};$")
 
   for block in blist:
     # TD_PERFCOUNTER0_SELECT   <GpuF0Reg:0x36c00> <GpuF1Reg:0x36c00>   32    {
     #   "TD_PERF_SEL_td_sp_traffic  :  Count the number of times this TD sends data to the SP. " = 0x2e,
     #   "SPI_PERF_VS_POS0_STALL  :  Number of clocks stalled due to pos buf space in SH0. " = 0x6,
     #   "GRBM_PERF_SEL_CPAXI_BUSY  :  The CPAXI block is busy. " = 0x25 }  ;
-    beg_pattern = re.compile("^" + block + "_PERFCOUNTER0_SELECT\s")
-    rec_pattern = re.compile("\"([A-Z]*_PERF_SEL_|SPI_PERF_)(\w*)\s*:?\s*(\w?[^\"]*)\s*\"\s*=\s*(\w*)(,|\s*\}\s*;)")
-  
+    # block prefix formats: TD_PERFCOUNTER0_SELECT, GRBM_SE0_PERFCOUNTER_SELECT, SDMA0_PERFMON_CNTL
+    beg_pattern = re.compile("^" + block + "(_PERFCOUNTER0_SELECT|0_PERFCOUNTER_SELECT|0_PERFMON_CNTL)\s")
+    # event prefix formats: TCP_PERF_SEL_, GRBM_SE0_PERF_SEL, SPI_PERF_
+    event_expr= "\"" + block + "([0C]?_PERF_SEL_|_PERF_)(\w*)\s*:?\s*(\w?[^\"]*)\s*\"\s*=\s*(\w*)"
+    # pattern of non-final event
+    rec_pattern = re.compile(event_expr + ",")
+    # pattern of final event
+    last_pattern = re.compile(event_expr + "\s*\}\s*;")
+
     found = 0
     record = ""
-    line_num = -1 
+    line_num = -1
     inp.seek(0, os.SEEK_SET)
     for line in inp.readlines():
+      # stop when coming to a line signaling the block end
+      # only take effect when the last_pattern was not captured
+      if found and end_pattern.search(line): break;
+
       record += line[:-1]
-  
+
       line_num += 1
       if len(record) > REC_MAX_LEN:
         print "Error: bad record \"" + record + "\"\nfile '" + raifile + ", line (" + str(line_num) + ")"
         break;
-  
-  
+
+
       if found:
-        if end_pattern.search(record):
-          # there might be a final event to extract
-          parse_event(rec_pattern, record, block, out)
+        if last_pattern.search(record):
+          # final event for the block, extract and stop
+          parse_event(last_pattern, record, block, out)
           break
         else:
           match = parse_event(rec_pattern, record, block, out)
@@ -74,9 +87,9 @@ def parse_rai(inp, out, blist):
         found = 1
         print line
         out.write("  # " + block + " counters\n")
-  
+
       record = ""
- 
+
     if not found:
       print >>sys.stderr, "Error: block '" + block + " not found"
       #break
