787977c64774754872a1559be071b57527f4ff01
Evgeny <evgeny.shcherbakov@amd.com>
2020-03-27 17:49:24 -0500

    clang 11 build change
    Change-Id: I0c7c71071daf8ba1149910d709411a659e977af6

--- script/build_kernel.sh
+++ script/build_kernel.sh
@@ -31,19 +31,28 @@ fi
 OCL_VER="2.0"
 OCL_DIR=$ROCM_DIR/opencl
 
-LLVM_DIR=$ROCM_DIR/hcc
-CLANG=$LLVM_DIR/bin/clang
-INC_DIR=$LLVM_DIR/lib/clang/10.0.0/include
+if [ -e $ROCM_DIR/llvm ] ; then
+  LLVM_DIR=$ROCM_DIR/llvm
+  LIB_DIR=$ROCM_DIR/lib
+  CLANG_VER="11.0.0"
+else
+  LLVM_DIR=$ROCM_DIR/hcc
+  LIB_DIR=$LLVM_DIR/lib
+  CLANG_VER="10.0.0"
+fi
+
+BIN_DIR=$LLVM_DIR/bin
+INC_DIR=$LLVM_DIR/lib/clang/$CLANG_VER/include
 BITCODE_OPTS="\
-  -Xclang -mlink-bitcode-file -Xclang $LLVM_DIR/lib/opencl.amdgcn.bc \
-  -Xclang -mlink-bitcode-file -Xclang $LLVM_DIR/lib/ockl.amdgcn.bc \
-  -Xclang -mlink-bitcode-file -Xclang $LLVM_DIR/lib/ocml.amdgcn.bc"
+  -Xclang -mlink-bitcode-file -Xclang $LIB_DIR/opencl.amdgcn.bc \
+  -Xclang -mlink-bitcode-file -Xclang $LIB_DIR/ockl.amdgcn.bc \
+  -Xclang -mlink-bitcode-file -Xclang $LIB_DIR/ocml.amdgcn.bc"
 
 for GFXIP in $TGT_LIST ; do
   OBJ_PREF=$GFXIP
   OBJ_NAME=$(echo "_$(basename $TEST_NAME)" | sed -e 's/_./\U&\E/g' -e 's/_//g')
   OBJ_FILE=${OBJ_PREF}_${OBJ_NAME}.hsaco
-  $CLANG -cl-std=CL$OCL_VER -include $INC_DIR/opencl-c.h $BITCODE_OPTS -target amdgcn-amd-amdhsa -mcpu=$GFXIP -mno-code-object-v3 $TEST_NAME.cl -o $DST_DIR/$OBJ_FILE
+  $BIN_DIR/clang -cl-std=CL$OCL_VER -include $INC_DIR/opencl-c.h $BITCODE_OPTS -target amdgcn-amd-amdhsa -mcpu=$GFXIP -mno-code-object-v3 $TEST_NAME.cl -o $DST_DIR/$OBJ_FILE
   echo "'$OBJ_FILE' is generated for '$GFXIP'"
 done
 
