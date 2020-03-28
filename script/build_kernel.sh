3bff25e2852ca087afc96fdefac654cdb9f4d73b
Evgeny <evgeny.shcherbakov@amd.com>
2020-03-28 14:55:05 -0500

    added search for LLVM clang library available
    Change-Id: I2afa0f02f28d24a1927bca8da967a8185f1d4974

--- script/build_kernel.sh
+++ script/build_kernel.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -x
+SO_EXT="hsaco"
 
 TEST_NAME=$1
 DST_DIR=$2
@@ -7,9 +8,10 @@ TGT_LIST=$4
 
 if [ -z "$TEST_NAME" ] ; then
   echo "Usage: $0 <test name> <dst dir>"
-  echo "  Will look for <test name>.cl and will build <test name>.so dynamic object library"
+  echo "  Will look for <test name>.cl and will build <test name>.$SO_EXT dynamic code object library"
   exit 1
 fi
+OBJ_NAME=$(echo "_$(basename $TEST_NAME).$SO_EXT" | sed -e 's/_./\U&\E/g' -e 's/_//g')
 
 if [ -z "$DST_DIR" ] ; then
   DST_DIR=$(dirname TEST_NAME)
@@ -29,20 +31,24 @@ if [ -z "$TGT_LIST" ] ; then
 fi
 
 OCL_VER="2.0"
-OCL_DIR=$ROCM_DIR/opencl
 
 if [ -e $ROCM_DIR/llvm ] ; then
   LLVM_DIR=$ROCM_DIR/llvm
   LIB_DIR=$ROCM_DIR/lib
-  CLANG_VER="11.0.0"
 else
   LLVM_DIR=$ROCM_DIR/hcc
   LIB_DIR=$LLVM_DIR/lib
-  CLANG_VER="10.0.0"
+fi
+
+CLANG_ROOT=$LLVM_DIR/lib/clang
+CLANG_DIR=`ls -d $CLANG_ROOT/* | head -n 1`
+if [ "$CLANG_DIR" = "" ] ; then
+  echo "Error: LLVM clang library was not found"
+  exit 1
 fi
 
 BIN_DIR=$LLVM_DIR/bin
-INC_DIR=$LLVM_DIR/lib/clang/$CLANG_VER/include
+INC_DIR=$CLANG_DIR/include
 BITCODE_OPTS="\
   -Xclang -mlink-bitcode-file -Xclang $LIB_DIR/opencl.amdgcn.bc \
   -Xclang -mlink-bitcode-file -Xclang $LIB_DIR/ockl.amdgcn.bc \
@@ -50,10 +56,9 @@ BITCODE_OPTS="\
 
 for GFXIP in $TGT_LIST ; do
   OBJ_PREF=$GFXIP
-  OBJ_NAME=$(echo "_$(basename $TEST_NAME)" | sed -e 's/_./\U&\E/g' -e 's/_//g')
-  OBJ_FILE=${OBJ_PREF}_${OBJ_NAME}.hsaco
+  OBJ_FILE=${OBJ_PREF}_${OBJ_NAME}
   $BIN_DIR/clang -cl-std=CL$OCL_VER -include $INC_DIR/opencl-c.h $BITCODE_OPTS -target amdgcn-amd-amdhsa -mcpu=$GFXIP -mno-code-object-v3 $TEST_NAME.cl -o $DST_DIR/$OBJ_FILE
-  echo "'$OBJ_FILE' is generated for '$GFXIP'"
+  echo "'$OBJ_FILE' generated"
 done
 
 exit 0
