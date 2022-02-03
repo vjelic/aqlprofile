2ec8965983952c4baae4d9d638ef40770eb7f8d0
Kiumars Sabeti <Kiumars.Sabeti@amd.com>
2022-02-03 15:25:06 -0500

    replace the set of CMAKE_SHARED_LINKER_FLAGS with add_link_options.
    Change-Id: I883d7a3668de168168875782117cc348236d6019

--- cmake_modules/env.cmake
+++ cmake_modules/env.cmake
@@ -29,7 +29,7 @@ set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fms-extensions" )
 set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fmerge-all-constants" )
 set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC" )
 
-set ( CMAKE_SHARED_LINKER_FLAGS "-Wl,-Bdynamic -Wl,-z,noexecstack" )
+add_link_options ("-Bdynamic -z,neexecstack")
 
 set ( CMAKE_SKIP_BUILD_RPATH TRUE )
 
