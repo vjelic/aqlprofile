93b46c936758446698715c7460deff4bb2215582
Ammar ELWazir <Ammar.ELWazir@amd.com>
2022-03-05 06:58:31 +0000

    SWDEV-296010: Fixing RPATH & CMAKE files
    Removing the RPATH skip & Removing the export line in the build.sh as it is not needed, libraries are found using find_library in the /opt/rocm path
    
    Change-Id: Ic2dbf6e14002348d73065d17dedd630cbb2c1b4b

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 BIN_DIR=`dirname $0`
 BLD_DIR=$BIN_DIR/build
 
-export CMAKE_PREFIX_PATH=/opt/rocm/lib:/opt/rocm/include/hsa
-rm -rf $BLD_DIR && mkdir $BLD_DIR && cd $BLD_DIR && cmake ..
+rm -rf $BLD_DIR && mkdir $BLD_DIR && cd $BLD_DIR && cmake -DCPACK_GENERATOR="DEB;RPM" ..
 make -j
 if [ "$?" = 0 ] ; then
   make mytest
--- cmake_modules/env.cmake
+++ cmake_modules/env.cmake
@@ -31,8 +31,6 @@ set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC" )
 
 add_link_options ("-Bdynamic -z,neexecstack")
 
-set ( CMAKE_SKIP_BUILD_RPATH TRUE )
-
 add_definitions ( -DNEW_TRACE_API=1 )
 
 ## CLANG options
