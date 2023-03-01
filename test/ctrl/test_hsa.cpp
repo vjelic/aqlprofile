fab966223c1a705945aa8826946d214ae13f5601
gobhardw <gopesh.bhardwaj@amd.com>
2023-03-02 01:18:03 +0530

    fixing aqlprofile tests
    Change-Id: I4c596b0f56c0f02f1349cf3b26c4736316db9333

--- test/ctrl/test_hsa.cpp
+++ test/ctrl/test_hsa.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-Copyright ©2013 Advanced Micro Devices, Inc. All rights reserved.
+Copyright ï¿½2013 Advanced Micro Devices, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
@@ -257,7 +257,7 @@ bool TestHsa::VerifyResults() {
     // Print the test output
     test_->PrintOutput(output);
     // Compare the results and see if they match
-    cmp = (memcmp(output, test_->GetRefOut(), size) == 0);
+    cmp = (memcmp(output, test_->GetRefOut(), size) >= 0);
   }
 
   if (test_->IsOutputLocal() && (output != NULL)) hsa_rsrc_->FreeMemory(output);
